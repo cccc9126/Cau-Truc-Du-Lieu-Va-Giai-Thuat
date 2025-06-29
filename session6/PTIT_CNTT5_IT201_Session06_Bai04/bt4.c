46ba945b7d0ea5cf4629c7e1c58fd7a1d7349fbc
Cachiu <cachiu@Cachius-MacBook-Air.local>
2025-06-29 12:45:12 +0700

    update

--- session6/PTIT_CNTT5_IT201_Session06_Bai04/bt4.c
+++ session6/PTIT_CNTT5_IT201_Session06_Bai04/bt4.c
@@ -2,16 +2,15 @@
 
 
 void towerOfHanoi(int n, char from, char aux, char to) {
-    if (n == 1) {
-        printf("Di chuyen dia 1 tu %c sang %c\n", from, to);
-        return;
-    }
-    towerOfHanoi(n - 1, from, to, aux);
-    printf("Di chuyen dia %d tu %c sang %c\n", n, from, to);
-    towerOfHanoi(n - 1, aux, from, to);
+ if (n==1) {
+     printf("Di chuyen dia 1 tu %c qua %c", from, to);
+ }
+    towerOfHanoi(n-1, from, to,aux);
+    printf("Di chuyen dia %d tu %c qua %c", n, from, to);
+    towerOfHanoi(n-1, aux, from, to);
 }
 int main() {
-    int n = 3;
+    int n = 1;
     printf("Cac buoc di chuyen %d dia:\n", n);
     towerOfHanoi(n, 'A', 'B', 'C');
     return 0;
